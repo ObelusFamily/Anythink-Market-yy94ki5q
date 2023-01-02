30a9790477023e3d29655f9b680262d4de88b834
Terence Johnson <tejohn@gmail.com>
2023-01-02 11:33:00 -0500

    fix for title search capability to API

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -54,7 +54,7 @@ router.get("/", auth.optional, function(req, res, next) {
   }
   
   if (typeof req.query.title !== "undefined") {
-    const text = new RegExp(req.query.title,i)
+    const text = new RegExp(req.query.title,'i')
     query.title = { $text:text };
   }
 
